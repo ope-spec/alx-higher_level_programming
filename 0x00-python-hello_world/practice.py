83b52db64407768842835720a5473a570eacaacc
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-08 10:37:13 +0100

    Still working

---
+++ 0x00-python-hello_world/practice.py
@@ -0,0 +1,11 @@
+a = "Python is cool"
+print(a[-5])
+
+"""
+This is a multi-line comment
+
+"""
+
+# Also note that space in python is seen as string
+# This is a single line comment
+# Life is so easy with python
