14c302013db7cae62fcacc5e718950d71c0e24e1
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-09 18:29:44 +0100

    Task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a')-1, -1):
-    print('{0}'.format(chr(i).upper() if (i-ord('a'))%2==0 else chr(i).lower()), end='')
+for i in range(ord('z'), ord('a') - 1, -1):
+    char = chr(i).upper() if (i - ord('a')) % 2 == 0 else chr(i).lower()
+    print('{0}'.format(char), end='')
