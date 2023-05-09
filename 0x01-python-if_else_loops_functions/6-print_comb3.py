1b8b695ff3c4f7f11e87a4147e2fd127ff4b780e
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-09 17:25:17 +0100

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 10):
     for j in range(i + 1, 10):
-        if i != 0 or j != 1:
-            print(", ", end="")
-        print(f"{i}{j}", end="")
-print()
+        if i == 8 and j == 9:
+            print('89')
+        else:
+            print(f"{i}{j}", end="")
+            if i != 8 or j != 9:
+                print(", ", end="")
