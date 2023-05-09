bc1a633381e09e47b126f9690cdb15dd92e0c919
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-09 11:08:51 +0100

    Upload test file

---
+++ Test Folder/Practice1.py
@@ -0,0 +1,35 @@
+for i in range(2, 4):
+    print(i, end=" ")
+    print ("\n")
+
+a = 12
+if a < 2:
+    print("Holberton\n")
+elif a % 2 == 0:
+    print("C is fun\n")
+else:
+    print("School\n")
+
+
+if 12 == 48/4 and False:
+    print("Holberton")
+else:
+    print("School")
+
+
+if 12 == 48/4:
+    print("Holberton\n")
+else:
+    print("School\n")
+
+for i in range(4):
+    print(i, end=" ")
+
+a = 12
+if a > 2:
+    if a % 2 == 0:
+        print("Holberton")
+    else:
+        print("C is fun")
+else:
+    print("School")
