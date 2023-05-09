987868ade8ea7a759639abca71eca1324bc63182
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-09 17:59:51 +0100

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(s):
-    output = ""
+    result = ""
     for c in s:
         if ord('a') <= ord(c) <= ord('z'):
-            output += '{}'.format(chr(ord(c) - 32))
+            result += '{}'.format(chr(ord(c) - 32))
         else:
-            output += '{}'.format(c)
-    print('{}'.format(output))
+            result += '{}'.format(c)
+    print('{}'.format(result))
