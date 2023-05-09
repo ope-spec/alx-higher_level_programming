9ad8517ce3d8a0ffd3b4fe421fdebdedc4c76a78
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-09 16:31:38 +0100

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char_code in range(ord('a'), ord('z') + 1):
     if chr(char_code) not in {'e', 'q'}:
-        print(chr(char_code), end='')
+        print('{:c}'.format(char_code), end='')
