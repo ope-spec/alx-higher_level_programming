384440e36465d640f747fa0353d3b07f6f5414ed
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-11 23:33:42 +0100

    Adjusted codes

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+import os.path
+if os.path.exists("hidden_4.pyc"):
+    os.remove("hidden_4.pyc")
+
 if __name__ == "__main__":
     import hidden_4
 
-for name in sorted(dir(hidden_4)):
-    if not name.startswith("__"):
-        print("{}".format(name))
+    for name in sorted(dir(hidden_4)):
+        if not name.startswith("__"):
+            print("{}".format(name))
