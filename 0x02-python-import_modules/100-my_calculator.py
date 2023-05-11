630264dd226741600b9fc1752f87422192ca1e9f
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-12 00:09:20 +0100

    Adjusted advance task 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,25 +4,15 @@ if __name__ == "__main__":
     import sys
 from calculator_1 import add, sub, mul, div
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <cal_method> <b>")
+if len(sys.argv) - 1 != 3:
+    print("Usage: ./100-my_calculator.py <operand1> <operator> <operand2>")
     sys.exit(1)
 
-a = int(sys.argv[1])
-cal_method = sys.argv[2]
-b = int(sys.argv[3])
-
-output = None
-if cal_method == '+':
-    output = add(a, b)
-elif cal_method == '-':
-    output = sub(a, b)
-elif cal_method == '*':
-    output = mul(a, b)
-elif cal_method == '/':
-    output = div(a, b)
-else:
-    print("Unknown cal_method. Available cal_methods: +, -, * and /")
+operators = {"+": add, "-": sub, "*": mul, "/": div}
+if sys.argv[2] not in list(operators.keys()):
+    print("Unknown operator. Available operators: +, -, * and /")
     sys.exit(1)
 
-print("{} {} {} = {}".format(a, cal_method, b, output))
+operand1 = int(sys.argv[1])
+operand2 = int(sys.argv[3])
+print("{} {} {} = {}".format(operand1, sys.argv[2], operand2, operators[sys.argv[2]](operand1, operand2)))
