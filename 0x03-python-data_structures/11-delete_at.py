51f0a0d473e3a8a0f6c1e3633a39efd43dbcc4c2
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-15 23:50:08 +0100

    Task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,4 +3,10 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    return my_list[:idx] + my_list[idx+1:]
+    
+    new_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+    
+    return new_list
