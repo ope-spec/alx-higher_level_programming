246de2c3060301b176a288692cc40ff8ea9de59c
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-17 18:20:22 +0100

    Advanced task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    sum_product = sum(score * weight for score, weight in my_list)
+    sum_weights = sum(weight for _, weight in my_list)
+
+    return sum_product / sum_weights
