e39f008823e1059d7bd1fd1d4bfb5f6d14cefa0b
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-17 17:39:47 +0100

    Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    print("Updated dictionary: {}".format(a_dictionary))
-    return a_dictionary
+    return (a_dictionary)
