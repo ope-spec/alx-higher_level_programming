6178b39e1bedab5c514a1a05c753c2fc91997228
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-17 17:00:58 +0100

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    rows = len(matrix)
-    cols = len(matrix[0])
-    result = [[0] * cols for _ in range(rows)]
-    
-    for i in range(rows):
-        for j in range(cols):
-            result[i][j] = matrix[i][j] ** 2
-    
-    return result
+    return [[x ** 2 for x in row] for row in matrix]
