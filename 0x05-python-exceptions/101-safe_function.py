75cebbd026719129595d41ae24c5e644d8599fa3
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-29 20:32:23 +0100

    Tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(*args)
+        return fct(*args)
     except Exception as e:
-        print("Exception:", e, file=sys.stderr)
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
