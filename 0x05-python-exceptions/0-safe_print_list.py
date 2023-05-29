b3d4de5e0a7cabe98e5c72d9dffd406c8b9b855e
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-29 18:15:45 +0100

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    
+    list_count = 0
     try:
-        for element in my_list:
-            print(element, end=' ')
-            
-            count += 1
-            
-            # Check if the required number of elements has been printed
-            if count == x:
-                break
-    
-    except TypeError:
-        print("Error: Unable to print the list elements.")
-    
-    print()  # Print a new line
-    
-    return count
+        for i in range(x):
+            print(f"{my_list[i]}", end="")
+            list_count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return list_count
