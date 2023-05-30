3d4784d7bb4cdceb1eedc04951bbd8e363bb6e12
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-30 19:25:38 +0100

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 
+"""class Square that defines a square by: (based on 1-square.py)"""
+
 
 class Square:
     """A class representing a square"""
-    
+
     def __init__(self, size=0):
         """Initializes a new instance of the Square class.
-        Args: size - The size of the square. Defaults to 0.
+        Args:
+            size: The size of the square. Defaults to 0.
         Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+            TypeError: if size is not integer
+            ValueError: if size is less than 0
         """
-        
+
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
