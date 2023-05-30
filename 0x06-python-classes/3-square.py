5948066dd3d3b8a89c96e7e956ac4a77dd0859c1
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-30 19:30:18 +0100

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 
+"""__init__(self, size=0): Initializes a Square object with a given size.
+area(self): Calculates and returns the area of the square.
+"""
+
+
 class Square:
+    """A class representing a square."""
+
     def __init__(self, size=0):
+        """Initializes a Square object with a given size.
+        Args:
+            size: The size of the square (default: 0).
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
+
+        """
+        
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -10,4 +26,9 @@ class Square:
             self.__size = size
 
     def area(self):
+        """Calculates and returns the area of the square.
+        Returns:
+            int: The area of the square.
+        """
+        
         return self.__size ** 2
