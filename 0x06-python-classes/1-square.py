f5adefcbf38f885e676f5ddb99b4d7a45ea9901c
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-05-30 19:01:58 +0100

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+"""A class representing a square."""
+
+
 class Square:
+    """
+    Initializes a Square object with a given size.
+    
+    Args:
+		size (int): The size of the square.
+    """
     def __init__(self, size):
         self.__size = size
