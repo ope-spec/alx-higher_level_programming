4db3002d96ef478d6bbf8323498a389f224685a2
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-06-12 14:32:35 +0100

    Python Inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
 """class MyInt that inherits from int"""
 
 
 class MyInt(int):
-    """class that inherits from int and inverts the behavior of == and != operators"""
+    """
+    class that inherits from int and inverts
+    the behavior of == and != operators
+    """
 
     def __eq__(self, value):
         """Overrides the == operator with != behavior"""
