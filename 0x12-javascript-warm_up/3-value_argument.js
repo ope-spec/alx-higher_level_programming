728cbb9b2189675224339c4dc559d8bf2d66c0bf
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-08-14 13:03:49 +0100

    Updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
+const args = process.argv[2];
 
-if (args.length === 0) {
+if (typeof args === 'undefined') {
   console.log('No argument');
 } else {
-  console.log(args[0]);
+  console.log(args);
 }
