8d5d8cc57f9e4e3ecc401cdeb22f768bd682d8c9
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-08-14 13:15:51 +0100

    Updated

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -3,10 +3,13 @@ const myObject = {
   type: 'object',
   value: 12
 };
+
 console.log(myObject);
+
 myObject.incr = function () {
   this.value++;
 };
+
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
