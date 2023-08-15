3e75ab9036d9579c2ee8e57bd851cc16daf6996d
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-08-15 15:55:15 +0100

    Updated code

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,14 +2,9 @@
 
 const fs = require('fs');
 
-const file1 = process.argv[2];
-const file2 = process.argv[3];
-const destinationFile = process.argv[4];
-
-const file1Content = fs.readFileSync(file1, 'utf-8');
-const file2Content = fs.readFileSync(file2, 'utf-8');
-const concatContent = file1Content + file2Content;
-
+const [file1, file2, destinationFile] = process.argv.slice(2);
+const concatContent = fs.readFileSync(file1, 'utf-8') + fs.readFileSync(file2, 'utf-8');
 fs.writeFileSync(destinationFile, concatContent);
 
 console.log(`Contents of ${file1} and ${file2} have been concatenated to ${destinationFile}.`);
+
