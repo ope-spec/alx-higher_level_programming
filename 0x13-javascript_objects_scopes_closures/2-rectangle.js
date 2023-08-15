c29d133fa6a2d53e5b4ffd4ffb81f2c5487ba128
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-08-15 16:03:54 +0100

    Updated

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,9 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) { return {}; }
-    this.width = w;
-    this.height = h;
+    this.width = w; this.height = h;
   }
 }
-
 module.exports = Rectangle;
