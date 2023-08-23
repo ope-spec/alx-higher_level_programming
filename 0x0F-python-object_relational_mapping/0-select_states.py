890bb72cd9358a04881421f56e82bae2bd4b54aa
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-08-23 17:46:05 +0100

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+'''
+This script retrieves and lists all states
+from the hbtn_0e_0_usa database.
+'''
+if __name__ == "__main__":
+    sql = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3])
+
+    sql_cursor = sql.cursor()
+    sql_cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    state_records = sql_cursor.fetchall()
+
+    for state in state_records:
+        print(state)
+    sql_cursor.close()
+    sql.close()
