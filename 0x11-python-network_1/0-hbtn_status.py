80dfa7d0011750f94849d1807158b197aa5eca9b
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-01 14:22:19 +0100

    Updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,8 @@ https://alx-intranet.hbtn.io/status
 """
 
 
-import urllib.request
-
 if __name__ == '__main__':
+    import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
