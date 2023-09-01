984f7f9b5e81c76cd654a12b2132dba713e8c8c7
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-01 14:53:36 +0100

    Updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,11 +9,9 @@ found in the header of the response.
 import urllib.request
 import sys
 
-def get_request_id(url):
-    with urllib.request.urlopen(url) as res:
-        return res.headers.get("X-Request-Id")
-
 if __name__ == "__main__":
     url = sys.argv[1]
-    request_id = get_request_id(url)
-    print(request_id)
+
+    with urllib.request.urlopen(url) as res:
+        request_id = res.headers.get("X-Request-Id")
+        print(request_id)
