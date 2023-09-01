82417cdcf440775817565f0b82f6d9c2f4f8cb41
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-01 15:21:50 +0100

    Adjusted

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     request = urllib.request.Request(url)
     try:
-         with urllib.request.urlopen(url) as res:
-               print(res.read().decode("ascii"))
+        with urllib.request.urlopen(request) as res:
+            print(res.read().decode("ascii"))
     except urllib.error.HTTPError as e:
-        print("Error code:", e.code)
+        print("Error code: {}".format(e.code))
