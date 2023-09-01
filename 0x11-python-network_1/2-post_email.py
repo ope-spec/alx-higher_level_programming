51552643144718925106e95f6baffc0a151acf0a
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-01 15:00:11 +0100

    Updated

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,12 +8,10 @@ and displays the body of the response (decoded in utf-8)
 import urllib.request
 import sys
 
-def send_post_request(url, email):
-    with urllib.request.urlopen(url, data=urllib.parse.urlencode({"email": email}).encode()) as res:
-        return res.read().decode("utf-8")
+url = sys.argv[1]
+email = sys.argv[2]
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    res = send_post_request(url, email)
-    print(res)
+    with urllib.request.urlopen(url, data=urllib.parse.urlencode({"email": email}).encode()) as res:
+        res = res.read().decode("utf-8")
+        print(res)
