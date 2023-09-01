4e4ea259073789ea8fe50e896855b0560a18da18
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-01 15:14:11 +0100

    Adjusted

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,16 +7,10 @@ to the URL and displays the body of the response
 import urllib.request
 import sys
 
-def send_request(url):
-    with urllib.request.urlopen(url) as res:
-        if res.getcode() != 200:
-            raise urllib.error.HTTPError(res.getcode())
-        return res.read().decode("utf-8")
-
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
-        res = send_request(url)
-        print(res)
+         with urllib.request.urlopen(url) as res:
+               print(res.read().decode("ascii"))
     except urllib.error.HTTPError as e:
         print("Error code:", e.code)
