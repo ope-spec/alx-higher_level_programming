16fbc87bb7e3723275893b438cc7d03ac406af49
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-01 14:20:26 +0100

    Updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,13 +6,11 @@ https://alx-intranet.hbtn.io/status
 
 import urllib.request
 
-def fetch_status():
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+if __name__ == '__main__':
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
-
-if __name__ == "__main__":
-    fetch_status()
