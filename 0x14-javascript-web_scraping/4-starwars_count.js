25c62d0e42cc8c48c5a6c3e3d98850ea5b8632e3
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-26 18:07:33 +0100

    Updated

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,22 +2,27 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const characterId = 18; // Wedge Antilles character ID
+const url = process.argv[2];
 
-request(apiUrl, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Request failed with status code ${response.statusCode}`);
-  } else {
-    const filmsData = JSON.parse(body).results;
-    const count = filmsData.reduce((total, film) => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        return total + 1;
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    
+    for (const film of films) {
+      const filmChars = film.characters;
+      
+      for (const characterUrl of filmChars) {
+        if (characterUrl.includes('18')) {
+          count++;
+        }
       }
-      return total;
-    }, 0);
+    }
+    
     console.log(count);
+  } else {
+    console.error(`An error occurred. Status code: ${response.statusCode}`);
   }
 });
