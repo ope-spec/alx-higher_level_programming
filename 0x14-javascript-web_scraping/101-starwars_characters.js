7a58f9b03e427b671d1bc6a1981bd3a706cc9868
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-26 18:33:05 +0100

    Updated

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,35 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Request failed with status code ${response.statusCode}`);
-  } else {
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-
-    function fetchCharacterName(index) {
-      if (index < characterUrls.length) {
-        request(characterUrls[index], (characterError, characterResponse, characterBody) => {
-          if (characterError) {
-            console.error(characterError);
-          } else if (characterResponse.statusCode !== 200) {
-            console.error(`Character request failed with status code ${characterResponse.statusCode}`);
-          } else {
-            const characterData = JSON.parse(characterBody);
-            console.log(characterData.name);
-            fetchCharacterName(index + 1);
-          }
-        });
-      }
-    }
-
-    fetchCharacterName(0);
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+function printCharacters(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+  request(characters[index], (error, response, body) => {
+    if (!error) {
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+      printCharacters(characters, index + 1);
+    }
+  });
+}
