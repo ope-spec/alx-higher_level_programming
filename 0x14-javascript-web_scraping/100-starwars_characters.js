4d593bad6b53d595553be93f42efea77517d5c4c
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-26 18:29:17 +0100

    Updated

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,30 +2,23 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
 
-function fetchCharacterName(url) {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else if (response.statusCode !== 200) {
-      console.error(`Character request failed with status code ${response.statusCode}`);
-    } else {
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    }
-  });
-}
-request(apiUrl, (error, response, body) => {
+request.get(apiUrl + movieId, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Request failed with status code ${response.statusCode}`);
-  } else {
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-    characterUrls.forEach((url) => {
-      fetchCharacterName(url);
-    });
+    return;
   }
+  const movieData = JSON.parse(body);
+  const characterUrls = movieData.characters;
+  characterUrls.forEach((characterUrl) => {
+    request.get(characterUrl, (characterError, characterResponse, characterBody) => {
+      if (characterError) {
+        console.error(characterError);
+        return;
+      }
+      const characterData = JSON.parse(characterBody);
+      console.log(characterData.name);
+    });
+  });
 });
