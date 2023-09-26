f6106c22e471c1b5b45196b7b29c795e3ce82551
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-09-26 18:35:47 +0100

    Updated

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,16 +4,11 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(apiUrl, (error, response, body) => {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
 function printCharacters(characters, index) {
   if (index >= characters.length) {
     return;
   }
+
   request(characters[index], (error, response, body) => {
     if (!error) {
       const characterName = JSON.parse(body).name;
@@ -22,3 +17,10 @@ function printCharacters(characters, index) {
     }
   });
 }
+
+request(apiUrl, (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
